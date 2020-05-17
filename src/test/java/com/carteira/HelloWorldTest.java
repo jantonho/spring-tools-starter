53eb5527d346c896be9fa9207b77f0dc8f61edd8
oliveira.joao <oliveira.joao@NB-004282.cwinet.local>
2020-05-17 20:21:32 -0300

    refactory

--- src/test/java/com/carteira/HelloWorldTest.java
+++ src/test/java/com/carteira/HelloWorldTest.java
@@ -5,16 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class HelloWorldTest {
 
 	@Test
 	public void firstTest() {
-		System.out.println("validando configs de testes");
-		
 		assertEquals(1, 1);
 	}
 }
